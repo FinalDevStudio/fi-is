5f50f4f5fa0a9590554dd42803680e0f176acd1b
Santiago G. Marín <santiago@finaldevstudio.com>
2019-11-08 18:41:12 -0300

    Remove chalk dependency

--- postinstall.js
+++ postinstall.js
@@ -1,4 +1,2 @@
-const chalk = require('chalk');
-
-console.warn(chalk.yellow(`Fi Is: The "${chalk.bold('dayLightSavingTime')}" method was removed. Please use Moment.js for that.`));
+console.warn('Fi Is: The "dayLightSavingTime" method was removed. Please use Moment.js for that.');
 console.warn('https://momentjs.com/docs/#/query/is-daylight-saving-time/');
